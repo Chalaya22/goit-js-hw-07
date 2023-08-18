a768cb24a18aa2f47e9b8b0559fa7855ba94584d
Chalaya22 <chalayaolga22@gmail.com>
2023-08-18 13:41:34 +0300

    финиш

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function renderListGalary(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            data-source="${original}"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  renderListGalary(galleryItems)
+);
+
+galleryContainer.addEventListener("click", imgClickHandler);
+
+function imgClickHandler(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const modal = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}">`
+  );
+
+  modal.show();
+}
